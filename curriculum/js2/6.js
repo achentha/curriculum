905cc8a041e3662803c4861716cc683c3047018e
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-16 22:30:42 -0700

    Solve js2/6.js

--- curriculum/js2/6.js
+++ curriculum/js2/6.js
@@ -5,7 +5,22 @@
  */
 
 const solution = (a) => {
-  return [];
+/* ver 1 - for loop
+  let result = [];
+  for (let i = 0; i < a.length; i++)
+    if (a[i] > 5) result.push(a[i]);
+  return result;
+ */
+
+/* ver 2 - forEach()
+  let result = [];
+  a.forEach(el => { if (el>5) result.push(el);});
+  return result;
+ */
+
+  /* ver 3 - filter() */
+  return a.filter(el => el > 5);
+
 };
 
 module.exports = {
