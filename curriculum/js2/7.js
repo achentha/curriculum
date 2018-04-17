5dbbf405c07f122f1b575a4a07b9f55f8b68e5ae
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-16 23:18:05 -0700

    Solve js2/7.js

--- curriculum/js2/7.js
+++ curriculum/js2/7.js
@@ -7,7 +7,25 @@
  */
 
 const solution = (a, b) => {
-  return 0;
+/* ver 1 - for loop
+  let count = 0;
+  for (let i = 0; i < a.length; i++)
+    if (a[i] === b) count++;
+  return count;
+ */
+
+/* ver 2 - forEach()
+  let count = 0;
+  a.forEach(el => {if (el === b) count++});
+  return count;
+ */
+
+  /* ver 3 - reduce() */
+  return a.reduce((count,el) => {
+    if (el === b) count++;
+    return count;
+  },0);
+
 };
 
 module.exports = {
