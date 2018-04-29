8e1cac277c734678ebfa3e0efef89710e39e8055
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-29 09:47:15 -0700

    Use recursive solution

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -4,10 +4,26 @@
  * @returns {number} max
  */
 
+/* ver 1 - reduce
 const solution = (obj)=>{
   let val = Object.values(obj);
   if (val.length === 0) return null;
   return val.reduce((max,el) => el>max? el : max);
+}
+*/
+
+/* ver 2 - recursive */
+const findMax = (arr,max) => {
+  if (!arr.length) return max;
+  if (arr[0] > max) max = arr[0];
+  return findMax(arr.slice(1),max);
+}
+
+const solution = (obj) => {
+  let val = Object.values(obj);
+  if (!val.length) return null;
+  else if (val.length === 1) return val[0];
+  return findMax(val, val[0]);
 }
 
 module.exports = {
