01552256b46886c1b94dacb336954a7a14aadf4e
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-28 22:43:15 -0700

    Change to recursive solution

--- curriculum/js2/6.js
+++ curriculum/js2/6.js
@@ -4,8 +4,8 @@
  * @returns {array}
  */
 
-const solution = (a) => {
 /* ver 1 - for loop
+const solution = (a) => {
   let result = [];
   for (let i = 0; i < a.length; i++)
     if (a[i] > 5) result.push(a[i]);
@@ -13,13 +13,24 @@ const solution = (a) => {
  */
 
 /* ver 2 - forEach()
+const solution = (a) => {
   let result = [];
   a.forEach(el => { if (el>5) result.push(el);});
   return result;
  */
 
-  /* ver 3 - filter() */
+/* ver 3 - filter()
+const solution = (a) => {
   return a.filter(el => el > 5);
+ */
+
+  /* ver 4 - recursive */
+const solution = (a,res=[]) => {
+  if (!a.length) return res;
+  if (a[0] > 5) res.push(a[0]);
+  return solution(a.slice(1), res);
+
+  
 
 };
 
