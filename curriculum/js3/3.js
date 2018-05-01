baa4c0d91b937efd8802b31ac4a3044cb519215e
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-30 22:48:10 -0700

    Use recursive solution

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -4,6 +4,7 @@
  * @return {array} b
  */
 
+/* ver 1 - forEach() & filter()
 const solution = (a)=>{
   if (a.length < 2) return [];
 
@@ -13,6 +14,27 @@ const solution = (a)=>{
   let num = Object.keys(obj);
   let freq = Object.values(obj);
   return num.filter((el,idx) => freq[idx] > 1).map(el => parseInt(el));
+}
+*/
+
+/* ver 2 - recursive */
+const buildFreq = (a,obj={}) => {
+  if (!a.length) return obj;
+  obj[a[0]] = obj[a[0]] === undefined ? 1 : obj[a[0]]+1;
+  return buildFreq(a.slice(1), obj);
+}
+
+const findDup = (entry,res=[]) => {
+  if (!entry.length) return res;
+  if (entry[0][1] > 1) res.push(parseInt(entry[0][0]));
+  return findDup(entry.splice(1),res);
+}
+
+const solution = (a) => {
+  if (a.length < 2) return [];
+  let freq = buildFreq(a);
+  let entry = Object.entries(freq);  //entry[i][0] = num, entry[i][1] = freq
+  return findDup(entry);
 }
 
 module.exports = {
