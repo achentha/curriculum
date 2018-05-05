57ad354608af9baf995e3c6fa4297569119e46a4
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-05 15:20:54 -0700

    Use index i instead of slice()

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -12,7 +12,7 @@ const solution = (obj)=>{
 }
 */
 
-/* ver 2 - recursive */
+/* ver 2 - recursive
 const findMax = (arr,max) => {
   if (!arr.length) return max;
   if (arr[0] > max) max = arr[0];
@@ -24,6 +24,21 @@ const solution = (obj) => {
   if (!val.length) return null;
   else if (val.length === 1) return val[0];
   return findMax(val, val[0]);
+}
+*/
+
+/* ver 3 - using index i instead of slice() */
+const findMax = (arr, max=arr[0], i=1) => {
+  if (i >= arr.length) return max;
+  return findMax(arr, arr[i] > max? arr[i] : max, i+1);
+}
+
+const solution = (obj) => {
+  let val = Object.values(obj);
+  if (!val.length) return null;
+  else if (val.length === 1) return val[0];
+  return findMax(val);
+
 }
 
 module.exports = {
