1feebfdc3379b617a7b6d01e96e23e9fcff116af
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-05 00:05:16 -0700

    Solve js3/5.js

--- curriculum/js3/5.js
+++ curriculum/js3/5.js
@@ -5,8 +5,16 @@
  * @return {nothing}
  **/
 
+const execute = (keys,values,b) => {
+  if (!keys.length) return;
+  b(keys.shift(), values.shift());
+  execute(keys,values,b);
+}
+
 const solution = (a,b)=>{
-  return;
+  let keys = Object.keys(a);
+  let values = Object.values(a);
+  execute(keys,values,b);
 }
 module.exports = {
   solution
