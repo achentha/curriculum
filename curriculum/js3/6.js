9315bf339e5d4e12d8e9ed62cd6e1c0b9bfc2454
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-05 09:51:56 -0700

    Solve js3/6.js

--- curriculum/js3/6.js
+++ curriculum/js3/6.js
@@ -8,8 +8,16 @@
  * @param {function} b
  * @returns {object} c
 */
+const execute = (entArr,b,res={}) => {
+  if (!entArr.length) return res;
+  if (b(entArr[0][0],entArr[0][1]))
+    res[entArr[0][0]] = entArr[0][1];
+  return execute(entArr.slice(1),b,res);
+}
+
 const solution = (a,b)=>{
-  return c;
+  let entArr = Object.entries(a);
+  return execute(entArr,b);
 }
 module.exports = {
   solution
