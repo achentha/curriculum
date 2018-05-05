656e4ca6e20fce733cd94060b53293967fef9454
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-04 23:43:20 -0700

    Solve js3/4.js

--- curriculum/js3/4.js
+++ curriculum/js3/4.js
@@ -6,8 +6,31 @@
  * @returns {boolean}
  */
 
-const solution = (a,b)=>{
-  return boolean;
+const buildNumObjArr = (a,c=[]) => {
+  if (!a.length) return c;
+  c.push({[a[0]] : true});
+  a.shift();
+  return buildNumObjArr(a,c);
+}
+
+const findNum = (arr,target) =>{
+  if (!arr.length) return false;
+  if (arr[0][target] === true) return true;
+  return findNum(arr.slice(1),target);
+}
+
+const pairAdd2b = (arr,b) => {
+  if (arr.length < 2) return false;
+  let target = b - parseInt(Object.keys(arr[0]));
+  arr.shift();
+  if (findNum(arr,target)) return true;
+  return pairAdd2b(arr,b);
+}
+
+const solution = (a,b) => {
+  if (a.length < 2) return false;
+  let arr = buildNumObjArr(a);
+  return pairAdd2b(arr,b);
 }
 
 module.exports = {
