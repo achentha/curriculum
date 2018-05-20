2c8d29dc01297ff30c51ccbf0149ee9e8deed41c
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-20 15:40:12 -0700

    Solve e2e All Except You problem

--- curriculum/end2end/app.js
+++ curriculum/end2end/app.js
@@ -1,1 +1,17 @@
 // All your code should go here
+const fs = require('fs');
+
+const showOthers = (arr,i=0) => {
+  if (i === arr.length-1) return;
+  if (arr[i] !== 'badchen') console.log(arr[i]);
+  showOthers(arr,i+1);
+}
+
+const allExceptU = () => {
+  fs.readdir('/home', (err, users) => {
+    if (err) throw err;
+    showOthers(users);
+  });
+}
+
+allExceptU();
