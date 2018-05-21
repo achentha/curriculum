49880c449c960764bb2fa58a5b95886a55848283
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-20 18:25:38 -0700

    Solve e2e Write All Except You

--- curriculum/end2end/app.js
+++ curriculum/end2end/app.js
@@ -1,1 +1,24 @@
 // All your code should go here
+const fs = require('fs');
+
+const writeOthers = (arr,i=0) => {
+    if (i === arr.length-1) return;
+    if (arr[i] !== 'badchen') {
+      fs.appendFile('/home/badchen/users.txt', arr[i] + ',\n', (err) => {
+        if (err) throw err;
+      });
+    }
+    writeOthers(arr,i+1);
+}
+
+const allExceptU = () => {
+    fs.readdir('/home', (err, users) => {
+          if (err) throw err;
+          fs.unlink('/home/badchen/users.txt', (err) => {
+            if (err) throw err;
+          });
+          writeOthers(users);
+        });
+}
+
+allExceptU();
