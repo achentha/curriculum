2d43138dafa53b19ffefa0319a53f8fd18da8526
Apichai Chenthanakij <badchen@garagescript.org>
2018-05-20 18:36:43 -0700

    Solve e2e Express Yourself

--- curriculum/end2end/app.js
+++ curriculum/end2end/app.js
@@ -1,1 +1,11 @@
 // All your code should go here
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.get('/msg', (req,res) => {
+  res.send('hello');
+});
+
+app.listen(5286);
