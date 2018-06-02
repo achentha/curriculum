3f07962b05243e177f5abdae27295225976cedc5
Apichai Chenthanakij <badchen@garagescript.org>
2018-06-02 15:09:54 -0700

    partial solution

--- curriculum/js2/4.js
+++ curriculum/js2/4.js
@@ -4,9 +4,17 @@
  * @returns {number}
  */
 
+/* ver 1
 const solution = (a) => {
   if (!a.length) return '';
   return a.reduce((sum,el) => {return (sum+el)},0);
+};
+*/
+
+/* ver 2 - recursive */
+const solution = (a,i=0,sum=0) => {
+  if (i === a.length) return sum;
+
 };
 
 module.exports = {
